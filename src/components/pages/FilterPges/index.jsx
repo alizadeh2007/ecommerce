d1c4d524552551c14eaea3256d53c5cfb7bc6924
Khalil Alizadeh <alizadeh3054@gmail.com>
2022-07-28 11:40:46 +0400

    pagination done

--- src/components/pages/FilterPges/index.jsx
+++ src/components/pages/FilterPges/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Switch, Typography } from "@mui/material";
+import { Box, Pagination, Switch, Typography } from "@mui/material";
 import "./filterPage.Module.css";
 import MyCard from "../../customs/MyCard";
 import RangeSlider from "./RangeSlider/index";
@@ -7,25 +7,26 @@ import RadioButtonsGroup from "./CategoriesSection/index";
 import { useDispatch, useSelector } from "react-redux";
 import Sortes from "./Sortes/index";
 import { uptodateNewSort } from "../../../redux/slice/slice";
-
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import IconButton from "@material-ui/core/IconButton";
+// Filterpges is eyeGlass component
 function FilterPges() {
   const dispatch = useDispatch();
   const { searchEyesGlass } = useSelector((state) => state.searchEyesGlass);
   const { storeInfoJsonServer } = useSelector(
     (state) => state.storeInfoJsonServer
   );
-
-
-  
+  // create new data to sort in sortes component
   const { newSort } = useSelector((state) => state.newSort);
+
+  // search data
   useEffect(() => {
     dispatch(uptodateNewSort([...storeInfoJsonServer]));
-    console.log("inga hstam")
+    console.log("inga hstam");
     let finderCard = newSort.filter((item) =>
       item.title.includes(searchEyesGlass) ? item : null
     );
-
-
     if (searchEyesGlass) {
       dispatch(uptodateNewSort(finderCard));
     } else {
@@ -33,13 +34,41 @@ function FilterPges() {
     }
   }, [searchEyesGlass]);
 
-
+  // pagination
+  function paginate(array, page_size, page_number) {
+    return array.slice((page_number - 1) * page_size, page_number * page_size);
+  }
+  // state for change page in pagination
+  const [page, setPage] = useState("1");
+  const backBtn = function(){
+    if(+page>1){
+      setPage(+page-1)
+    } else{return setPage(+page)}
+  }
+  const forwardBtn = function(){
+    if(+page>=0){
+      setPage(Number(page)+1)
+    } else{return setPage(+page)}
+  }
   return (
     <Box className="FilterPges">
-      <Box className="FilterPges-pages">
-        {newSort?.map((item) => (
-          <MyCard title={item.title} price={item.price} image={item.imgURL} />
-        ))}
+      <Box display="block!important" width="100%!important">
+        <Box className="FilterPges-pages" width="100%!important">
+          {paginate(newSort, 12, +page)?.map((item) => (
+            <MyCard title={item.title} price={item.price} image={item.imgURL} />
+          ))}
+        </Box>
+        <Box display="flex" justifyContent="center" alignItems="flex-start">
+          <Box display="flex" alignItems="center" gap="2rem">
+            <IconButton onClick={backBtn}>
+              <ArrowBackIosNewIcon className="pagination-icon" />
+            </IconButton>
+            {page}
+            <IconButton onClick={forwardBtn}>
+              <ArrowForwardIosIcon className="pagination-icon" />
+            </IconButton>
+          </Box>
+        </Box>
       </Box>
       <Box className="FilterPges-filters">
         <Typography className="FilterPges-filters-title">فیلتر</Typography>
@@ -49,6 +78,7 @@ function FilterPges() {
         <Box className="FilterPges-filters-Available" width="100%!important">
           <Sortes width="100%!important" />
         </Box>
+
         <Box className="FilterPges-filters-Available">
           <Typography className="FilterPges-filters-Available-title">
             فقط سوپر مارکتی{" "}
